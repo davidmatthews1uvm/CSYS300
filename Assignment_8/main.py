88c2986c24022f34800212191d26229866b8d2f6
David Matthews <dmatthe1@uvm.edu>
2020-11-06 12:48:55 -0500

    updating hw 8 main.py to output some plots

--- Assignment_8/main.py
+++ Assignment_8/main.py
@@ -38,19 +38,22 @@ def candidate_tree_cost(tree_index, world, ag_id_sizes, ag_id_prob, cost, spark_
 
 
 l_b = 1/10
-def build_forest(L, D, l_b=1/10, container=None, stream=None):
+def build_forest(L, D, l_b=1/10, container=None, stream=None, early_termination=True):
     l = L*l_b
     spark_probs = np.fromfunction(lambda i, j: np.exp(-(i+j+2)/l), shape=(L,L), dtype=np.float64)
     spark_probs /= np.sum(spark_probs) # normalize the probability.
 
     world = np.zeros((L, L), dtype=np.int32) # keep track of the trees in the world.
+    best_world = np.zeros((L, L), dtype=np.int32)
+    best_world_saved = False
     ag_id_sizes = np.zeros((L*L), dtype=np.int32) # keep track of each forest size
+    ag_id_sizes_history = np.zeros((L*L, L*L), dtype=np.int32)
     ag_id_prob = np.zeros((L*L), dtype=np.float64) # keep track of each forest burn probability
     next_forest_id = 1
 
+    yield_history = np.zeros((L*L), dtype=np.float64)
     cost = 0
     old_cost = cost
-
 
     for iteration_num in tqdm(range(L*L)):
         if container is not None and stream is not None:
@@ -80,33 +83,38 @@ def build_forest(L, D, l_b=1/10, container=None, stream=None):
         min_spot_neighbors = None
         for t_id in trees_to_test:
             new_cost, neighbors = candidate_tree_cost(empty_spots[t_id], world, ag_id_sizes, ag_id_prob, cost, spark_probs)
-            # x, y  = empty_spots[t_id]
+            x, y  = empty_spots[t_id]
             # if ((x == L-3 and y == L-1) or (y == L-3 and x == L-1) or (x == L-2 and y == L-2)):
             #     print(x, y, new_cost)
             # if (iteration_num == 202):
             #     print(t_id, empty_spots[t_id], new_cost, neighbors)
-            if (new_cost < min_new_cost):
+            if ((new_cost < min_new_cost)): # IF USING MIDDLE TO EDGE TIE BREAKING or (new_cost == min_new_cost and abs(x-y) < abs(empty_spots[min_new_spot][0] - empty_spots[min_new_spot][1]))):
                 min_new_spot = t_id
                 min_spot_neighbors = neighbors
                 min_new_cost = new_cost
-        if (min_new_cost  > cost + 1):
-            print(old_cost, min_new_cost, cost, iteration_num, neighbors, min_new_spot, empty_spots[min_new_spot])
-            # if we have not already saved the current world to the video, do so now.
-            if iteration_num%FRAME_SUBSAMPLE != 0:
-                img = (world!=0).astype(np.uint8).reshape((*world.shape, 1))*255
-                img = np.repeat(img, TILE_SIZE, axis=0)
-                img = np.repeat(img, TILE_SIZE, axis=1)
-                img = np.repeat(img, 3, axis=2)
-                frame = av.VideoFrame.from_ndarray(img, format='rgb24')
-                for packet in stream.encode(frame):
-                    container.mux(packet)
-            return (cost, world, ag_id_sizes, ag_id_prob)
-        
+
+        if (not best_world_saved and min_new_cost  > cost + 1  ):
+            best_world[:] = world[:]
+            best_world_saved = True
+            if (early_termination):
+                # print(old_cost, min_new_cost, cost, iteration_num, neighbors, min_new_spot, empty_spots[min_new_spot])
+                # if we have not already saved the current world to the video, do so now.
+                if container is not None and stream is not None and iteration_num%FRAME_SUBSAMPLE != 0:
+                    img = (world!=0).astype(np.uint8).reshape((*world.shape, 1))*255
+                    img = np.repeat(img, TILE_SIZE, axis=0)
+                    img = np.repeat(img, TILE_SIZE, axis=1)
+                    img = np.repeat(img, 3, axis=2)
+                    frame = av.VideoFrame.from_ndarray(img, format='rgb24')
+                    for packet in stream.encode(frame):
+                        container.mux(packet)
+                return (cost, world, ag_id_sizes, ag_id_prob, yield_history, ag_id_sizes_history, best_world)
+
         old_cost = min_new_cost
         # print(min_new_cost, empty_spots[min_new_spot], min_spot_neighbors )
         forest_neighbors = sorted(set(min_spot_neighbors) - {0}, reverse=True)
         
         x, y  = empty_spots[min_new_spot]
+        ag_id_sizes_history[iteration_num] = ag_id_sizes # save the ag id size distribution
         if len(forest_neighbors) > 0:
             # print(world[x-1:x+2, y-1:y+2])
             # print(forest_neighbors, min_spot_neighbors)
@@ -117,6 +125,8 @@ def build_forest(L, D, l_b=1/10, container=None, stream=None):
             # print(min_forest_id)
             # print(forest_neighbors[:-1])
             world[x, y] = min_forest_id
+            ag_id_sizes[min_forest_id] +=1
+            ag_id_prob[min_forest_id] += spark_probs[x, y]
             for forest_id in forest_neighbors[:-1]:
                 # print(world)
                 # print(ag_id_sizes[0:next_forest_id+5])
@@ -157,43 +167,77 @@ def build_forest(L, D, l_b=1/10, container=None, stream=None):
             ag_id_sizes[next_forest_id] = 1
             ag_id_prob[next_forest_id] = spark_probs[x, y]
             next_forest_id += 1
+        yield_history[iteration_num] = 1 + iteration_num - cost
         cost = np.sum(ag_id_sizes * ag_id_prob)
         # if ( abs(cost-old_cost)/cost > 1e-16):
         # print(old_cost, cost, abs(cost-old_cost)/cost)
             # return ( cost, world, ag_id_sizes, ag_id_prob)
-    return (cost, world, ag_id_sizes, ag_id_prob)
+    print(next_forest_id )
+    return (cost, world, ag_id_sizes, ag_id_prob, yield_history, ag_id_sizes_history, best_world)
 
-global N, TILE_SIZE, FRAME_SUBSAMPLE
+global N
+# global TILE_SIZE, FRAME_SUBSAMPLE
 
-FPS = 60
-TARGET_DURATION = 10
+# FPS = 60
+# TARGET_DURATION = 20
 
-STREAM_SIZE = 1024
+# STREAM_SIZE = 1024
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--N", help="System Size", default=32, type=int)
-    args = parser.parse_args()
-    N = args.N
-    TILE_SIZE = STREAM_SIZE // N
-    assert STREAM_SIZE == TILE_SIZE * N
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--N", help="System Size", default=32, type=int)
+    # args = parser.parse_args()
+    # for n in range(2, args.N):
+    # N = n
+    # TILE_SIZE = STREAM_SIZE // N
+    # STREAM_SIZE = TILE_SIZE * N
 
-    FRAME_SUBSAMPLE = max(int((N**2 )/(FPS * TARGET_DURATION)), 1)
+    # FRAME_SUBSAMPLE = max(int((N**2 )/(FPS * TARGET_DURATION)), 1)
     
-    container = av.open('L{:d}D{:d}.mp4'.format(N, N**2), mode='w')
-    stream = container.add_stream('mpeg4', rate=FPS)
-    stream.width = STREAM_SIZE
-    stream.height = STREAM_SIZE
-    stream.pix_fmt = 'yuv420p'
+    # container = av.open('L{:d}D{:d}top.mp4'.format(N, N**2), mode='w')
+    # stream = container.add_stream('mpeg4', rate=FPS)
+    # stream.width = STREAM_SIZE
+    # stream.height = STREAM_SIZE
+    # stream.pix_fmt = 'yuv420p'
 
-    cost, world, ag_id_sizes, ag_id_prob = build_forest(N, N**2, container=container, stream=stream)
-    plt.imshow(world!=0)
-    plt.savefig("L{:d}D{:d}.png".format(N, N**2))
-    print(np.sum((world!=0)) - cost)
+    N = 64
+    data_dict = dict()
+    name_dict = {1:"1", 2:"2", N:"L", N*N:"L^2"}
+    for D in [1, 2, N, N*N]:
+        cost, world, ag_id_sizes, ag_id_prob,  yield_history, ag_id_sizes_history, best_world = build_forest(N, D, early_termination=False) #, container=container, stream=stream)
+        data_dict[D] = (cost, world, ag_id_sizes, ag_id_prob,  yield_history, ag_id_sizes_history, best_world)
+    
+    # Part 3a
+    fig, axs = plt.subplots(2,2, figsize=(9, 9))
+    (cost, world, ag_id_sizes, ag_id_prob,  yield_history, ag_id_sizes_history, best_world) = data_dict[1]
+    axs[0,0].imshow(best_world!=0, cmap=plt.cm.gray)
+    axs[0,0].set_title("$D=1$")
+    (cost, world, ag_id_sizes, ag_id_prob,  yield_history, ag_id_sizes_history, best_world) = data_dict[2]
+    axs[0,1].imshow(best_world!=0, cmap=plt.cm.gray)
+    axs[0,1].set_title("$D=2$")
+    (cost, world, ag_id_sizes, ag_id_prob,  yield_history, ag_id_sizes_history, best_world) = data_dict[N]
+    axs[1,0].imshow(best_world!=0, cmap=plt.cm.gray)
+    axs[1,0].set_title("$D=L$")
+    (cost, world, ag_id_sizes, ag_id_prob,  yield_history, ag_id_sizes_history, best_world) = data_dict[N*N]
+    axs[1,1].imshow(best_world!=0, cmap=plt.cm.gray)
+    axs[1,1].set_title("$D=L^2$")
+    plt.savefig("Part3a.pdf")
 
-    # Flush stream
-    for packet in stream.encode():
-        container.mux(packet)
+    # Part 3b
+    for D in data_dict.keys():
+        (cost, world, ag_id_sizes, ag_id_prob,  yield_history, ag_id_sizes_history, best_world) = data_dict[D]
+        plt.plot(np.linspace(0, 1, N*N), yield_history / (N*N), label="$D=%s$"%(name_dict[D]))
+        plt.ylabel("Yield")
+        plt.xlabel("Trees planted")
+        plt.legend()
+    plt.savefig("Part3b.pdf")
+    # plt.imshow(world!=0)
+    # plt.savefig("L{:d}D{:d}top.png".format(N, N**2))
+    # print(np.sum((world!=0)) - cost)
 
-    # Close the file
-    container.close()
+    # # Flush stream
+    # for packet in stream.encode():
+    #     container.mux(packet)
+
+    # # Close the file
+    # container.close()
